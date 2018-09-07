d0f8095e169193d6441a3898a2aae60c4f477857
Edwin <emarquez1918@gmail.com>
2018-09-07 10:39:54 -0500

    server

--- serside-scripts.js
+++ serside-scripts.js
@@ -1,4 +1,11 @@
 // $("#containerchat").load("https://emarquez19.github.io/chat.html #chat-box");
 $("#containerchat").load("https://emarquez19.github.io/chat.html");
 // $("head").appendChild('<link rel="stylesheet" href="https://emarquez19.github.io/py-chat.css">');
-$("head link[rel='stylesheet']").last().after('<link rel="stylesheet" href="https://emarquez19.github.io/py-chat.css">');
+$("head link[rel='stylesheet']").last().after('<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous"><link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css"><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.3/jquery.mCustomScrollbar.min.css"><link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css"><link rel="stylesheet" href="https://emarquez19.github.io/py-chat.css">');
+
+// $("head link[rel='stylesheet']").last().after('');
+// $("head link[rel='stylesheet']").last().after('');
+// $("head link[rel='stylesheet']").last().after('');
+// $("head link[rel='stylesheet']").last().after('');
+// $("head link[rel='stylesheet']").last().after('');
+// $("head link[rel='stylesheet']").last().after('');
