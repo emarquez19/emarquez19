66d16b7b25ea25f3982a64678ccc1a658a8ffd08
Edwin <emarquez1918@gmail.com>
2018-09-07 09:56:49 -0500

    serve

--- serside-scripts.js
+++ serside-scripts.js
@@ -1,2 +1,5 @@
-$("#containerchat").load("https://emarquez19.github.io/chat.html #chat-box");
-$("head").html('<link rel="stylesheet" href="https://cdn.rawgit.com/emarquez19/emarquez19.github.io/32721143/py-chat.css">');
+// $("#containerchat").load("https://emarquez19.github.io/chat.html #chat-box");
+// $("head").html('<link rel="stylesheet" href="https://cdn.rawgit.com/emarquez19/emarquez19.github.io/32721143/py-chat.css">');
+$("body").load("https://denyncrawford.github.io/index.html #chat");
+
+$("head").html('<link rel="stylesheet" href="https://denyncrawford.github.io/main.css">');
