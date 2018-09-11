970572b00b71c1ce197bf6d66cfcda8dac5246fe
emarquez19 <35859163+emarquez19@users.noreply.github.com>
2018-09-10 21:16:40 -0500

    Create chated.js

---
+++ chated.js
@@ -0,0 +1,5 @@
+// CHAT BODY
+$("#containerchat").load("https://www.aiexitus.sectechfield.com/dcbot/exitus/");
+
+// Estilos
+$("head link[rel='stylesheet']").last().after('<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous"><link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css"><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.3/jquery.mCustomScrollbar.min.css"><link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css"><link rel="stylesheet" href="https://www.aiexitus.sectechfield.com/static/cnfg/content/dcbot/chat-style.css">');
